e06fa8c28b6d4a51a5819b88f800f4cba43bc958
Akshit-Verma <akshitverma.dev@gmail.com>
2026-08-14 21:57:22 +0530

    corrected bubble sort
    Refactor bubble sort function for clarity and efficiency.

--- DSA python/bubble_sort.py
+++ DSA python/bubble_sort.py
@@ -1,10 +1,12 @@
-def Bubble_sort(lst:list) ->list:
-    for i in range(len(lst)):
+def bubble_sort(lst: list) -> list:
+    n = len(lst)
+    for i in range(n):
         swapped = False
-        for j in range(len(lst)):
-            if lst[j] > lst[j+1]:
-                lst[j], lst[j+1] = lst[j+1], lst[j]
+        for j in range(0, n - 1 - i):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
                 swapped = True
         if not swapped:
             break
-    return lst
+            
+    return ls
